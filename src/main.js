b7f3e5a1dd3c6bf2eed0c959781bf0469e38c8a8
hjz2016 <1073560118@qq.com>
2017-11-08 21:44:06 +0800

    hjz-11-8

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 公共样式
+import './style/common.scss'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
